4eaae222ded459058ce8da69ad13c95a67809a74
jidaikobo <support@jidaikobo.com>
2020-07-29 22:42:17 +0900

    style menu

--- parts/3.2.3_consistent-navigtion.php
+++ parts/3.2.3_consistent-navigtion.php
@@ -1,7 +1,9 @@
 <nav id="global-nav">
-<ul>
+<div id="wrapper-menu">
+<ul id="main-menu">
 	<li><a href="./">トップページ</a></li>
 	<li><a href="./register.php">会員登録</a></li>
 	<li><a href="./fact.php">温暖化の状況</a></li>
 </ul>
+</div>
 </nav>
--- parts/3.2.3_ok.php
+++ parts/3.2.3_ok.php
@@ -1,7 +1,10 @@
 <nav id="global-nav">
-<ul>
+<button type="button" aria-expanded="false" id="bttn-nav">メニュー</button>
+<div id="wrapper-menu">
+<ul id="main-menu">
 	<li><a href="./">トップページ</a></li>
 	<li><a href="./fact.php">温暖化の状況</a></li>
 	<li><a href="./register.php">会員登録</a></li>
 </ul>
+</div>
 </nav>
