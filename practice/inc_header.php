832ace60171f6cbdd746809c0cd948641dc995eb
jidaikobo <support@jidaikobo.com>
2020-09-29 19:08:26 +0900

    add faivcon

--- practice/inc_header.php
+++ practice/inc_header.php
@@ -4,3 +4,4 @@
 	<script src="//code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<link href="//use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 	<link href="//fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+	<link rel="icon" href="./images/favicon.ico" />
