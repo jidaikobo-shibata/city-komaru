a9e33328d3dcfe4a988e7442a895eecd0f53b1f9
jidaikobo <support@jidaikobo.com>
2020-10-02 18:36:51 +0900

    update search field keyboard trap

--- parts/2.1.2_no-keyboard-trap.php
+++ parts/2.1.2_no-keyboard-trap.php
@@ -1,4 +1,26 @@
 <form action="./do-not-test.php" method="GET">
-<input type="text" name="s" id="search-text" size="20" value="" onblur="$('#search').focus()">
-<input type="submit" id="search" value="検索" onblur="$('#search-text').focus()">
+<input type="text" name="s" id="search-text" size="20" value="">
+<input type="submit" id="search" value="検索">
 </form>
+<script>
+jQuery(function($){
+  const input = $('#search-text'),
+        bttn = $('#search'),
+        search_obj = input.add(bttn);
+  let flg = false;
+  search_obj.on('blur', function(){
+    if(flg){
+      flg = false;
+    }else{
+      search_obj.not(this).focus();
+    }
+  });
+  $('*').on('mousedown',function(e){
+    if($(this).is(search_obj)){
+      e.stopPropagation();
+      return;
+    }
+    flg = true;
+  });
+});
+</script>
