7afb587889aa6e1784481a7aa8ca7f2f0897cc0b
jidaikobo <support@jidaikobo.com>
2020-10-06 21:53:09 +0900

    modify slide ctrl

--- practice/index.php
+++ practice/index.php
@@ -25,8 +25,9 @@
 <?php // echoPracticeHtml('2.1.1b') ?>
 	<div id="main-slide">
 		<div id="slide-utilities">
-			<button type="button" id="slide-prev" class="bttn"><span class="visually-hidden">前のスライド</span></button>
-			<button type="button" id="slide-next" class="bttn"><span class="visually-hidden">次のスライド</span></button>
+			<button type="button" id="slide-prev" class="bttn slide-ctrl"><span class="visually-hidden">前のスライド</span></button>
+			<button type="button" id="slide-next" class="bttn slide-ctrl"><span class="visually-hidden">次のスライド</span></button>
+			<button type="button" id="slide-play" class="bttn bttn-pause fa fa-pause"><span class="visually-hidden pause">一時停止</span><span class="visually-hidden play">再生</span></button>
 		</div>
 		<div id="slide-wrapper">
 			<img src="./images/main-image-2_a.jpg" class="main-image" alt="スライド１：「すべての人にやさしいまちづくりを」持続可能な社会の実現に向け、気候変動問題への対応、廃棄物対策、生物多様性の保全など、さまざまな取組を行っています。" width="1366" height="520">
@@ -36,22 +37,36 @@
 	</div>
 <script>
 jQuery(function($){
-	const speed = 500,
+	const interval      = 7000,
+				speed         = 500,
 				slide_wrapper = $('#slide-wrapper'),
-				bttn = $('#slide-utilities').find('.bttn');
+				bttn_slide    = $('#slide-utilities').find('.slide-ctrl'),
+				bttn_play     = $('#slide-play');
 	let slides = slide_wrapper.find('.main-image'),
 			current = slides.eq(0),
-			flg = false;
+			flg = false,
+			dir = false,
+			timeoutID;
 	slide_wrapper.prepend(slides.not(current).clone()).append(current.clone());
 	slides = slide_wrapper.find('.main-image');
 	slides.not(current).attr({'aria-hidden':true});
 	slide_wrapper.addClass('on');
 
-	bttn.on('click', function(){
+	timeoutID = setTimeout(() => {slide_play()}, interval);
+	bttn_slide.on('click', slide_play);
+
+	bttn_play.on('click', function(){
+		$(this).toggleClass('bttn-play fa-play bttn-pause fa-pause');
+		flg = $(this).hasClass('bttn-play');
+		if(! flg){
+			slide_play()
+		}
+	});
+
+	function slide_play(){
 		if(flg) return;
 		flg = true;
-		const dir = this.id == 'slide-prev';
-// 自動再生して、再生・停止ボタンをつける
+		dir = event && event.target.id == 'slide-prev';
 		slides.css({
 			transform : 'translate3d('+(dir ? '' : '-')+'100%, 0, 0) ',
 			transition: 'transform '+speed+'ms ease-out'
@@ -69,7 +84,13 @@ jQuery(function($){
 			current.removeAttr('aria-hidden');
 			flg = false;
 		}, speed);
-	});
+
+		// for autoplay
+		if(!(event && $(event.target).hasClass('.slide-ctrl'))){
+			clearTimeout(timeoutID);
+			timeoutID = setTimeout(() => {slide_play()}, interval);
+		}
+	}
 });
 </script>
 </div>
