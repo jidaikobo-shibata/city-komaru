16058c91c1624ef24fc5eb33237a81c8483b84d9
jidaikobo <support@jidaikobo.com>
2020-10-07 16:52:11 +0900

    update test pattern disclosure

--- config.php
+++ config.php
@@ -10,6 +10,7 @@ setTestPatternCode(\Kontiki\Input::post('test_pattern_code', ''));
 <head>
 	<meta charset="utf-8">
 	<title>アクセシビリティ試験練習用サイト設定</title>
+	<script src="//code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 <body>
 <h1>アクセシビリティ試験練習用サイト設定</h1>
@@ -23,20 +24,12 @@ setTestPatternCode(\Kontiki\Input::post('test_pattern_code', ''));
 
 <h2>試験パターンコードを生成</h2>
 <form action="" method="POST">
-<?php
-$ng_checked = $ok_checked = ' checked="checked"';
-if (\Kontiki\Input::post('code_type') == 'ok'):
-	$ng_checked = '';
-else:
-	$ok_checked = '';
-endif;
-?>
+<?php $ng_checked = \Kontiki\Input::post('code_type') != 'individual' ? ' checked="checked"' : ''; ?>
 <ul>
 	<li><label><input type="radio" name="code_type" value="ng"<?php echo $ng_checked ?>> アクセシビリティ上の問題を含んだ試験パターンコードを生成</label></li>
-	<li><label><input type="radio" name="code_type" value="ok"<?php echo $ok_checked ?>> アクセシビリティ上の問題を解消した試験パターンコードを生成</label></li>
 <li>
-<details>
-<summary><label><input type="radio" name="code_type" value="individual"> バリアを個別に指定する</label></summary>
+<details id="individual_set">
+<summary tabindex="-1"><label><input type="radio" name="code_type" value="individual"> バリアを個別に指定する</label></summary>
 <?php
 $html = '';
 $html.= '<ul>';
@@ -78,5 +71,34 @@ echo $html;
 
 <p><a href="./">アクセシビリティ試験練習用サイトのトップページ</a></p>
 
+
+<script>
+$(function() {
+/*
+ * バリアを個別に指定する、を選択時、ディスクロージャオープン。
+ * 問題を含んだ〜を選択時には、ディスクロージャクローズ。
+ * もともとのディスクロージャの機能は殺す。
+ */
+	const radioButton = $('input[name="code_type"]')
+				detailsArea = $('#individual_set'),
+				detailsSummary = $('#individual_set summary');
+	radioButton.on({
+		'change' : toggleIndividualSet,
+		'click'  : function(){event.stopPropagation()}
+	});
+	detailsSummary.on('click', function(){
+		radioButton.eq(1).prop('checked', true).trigger('change');
+		return false;
+	});
+	function toggleIndividualSet(event){
+		target = !event ? radioButton.eq(1): $(event.target);
+		if(target.val() == 'individual'){
+			detailsArea.attr('open', '');
+		}else{
+			detailsArea.removeAttr('open');
+		}
+	}
+});
+</script>
 </body>
 </html>
