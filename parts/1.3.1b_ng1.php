204e69d92afb389f7e2acbc84baff569e6f78bad
jidaikobo <support@jidaikobo.com>
2020-10-08 16:20:30 +0900

    add .table style

--- parts/1.3.1b_ng1.php
+++ parts/1.3.1b_ng1.php
@@ -1,57 +1,5 @@
 <?php /* thであるべきtable */ ?>
-<style>
-table
-{
-	box-sizing: border-box;
-	border-collapse: collapse;
-	border-spacing: 0px;
-	background-color: #fff;
-	border-bottom: 1px #ccc solid;
-}
-table td
-{
-	text-align: left;
-	padding: 8px 10px;
-}
-table td.header
-{
-	color: #111;
-	background: #e8e8e0;
-	font-weight: bold;
-	border-top: 1px #ccc solid;
-	border-bottom: 1px #ccc solid;
-}
-
-table tr:nth-child(even),
-table.even tr:nth-child(even)
-{
-	background: #f4f4ea;
-}
-table tr:nth-child(odd),
-table.even tr:nth-child(odd)
-{
-	background: #fff;
-}
-table.odd tr:nth-child(odd)
-{
-	background: #fff;
-}
-table.odd tr:nth-child(even)
-{
-	background: #f4f4ea;
-}
-
-table td.header
-{
-	font-weight: bold;
-	color: #111;
-}
-table td
-{
-	color: #111;
-}
-</style>
-<table>
+<table class="table auto odd">
 <caption>各地の気温の上昇</caption>
 <tr>
 	<td class="header">都市</td>
--- parts/2.2.1a_ng1.php
+++ parts/2.2.1a_ng1.php
@@ -1,6 +1,6 @@
 <?php /* むりやり時間切れするフォーム */ ?>
 <script>
-const timeout_val = 10000;
+const timeout_val = 2 * 60 * 1000;
 var timeout = function() {
 				alert('入力時間が切れました');
 				document.registration.reset();
