cf1a5f377138ddc8bdb30f00866265b7132eee37
jidaikobo <support@jidaikobo.com>
2020-10-09 17:10:55 +0900

    nomarker list style

--- parts/1.3.1a_ng2.php
+++ parts/1.3.1a_ng2.php
@@ -1,11 +1,5 @@
 <?php /* NG？ 番号をコピーアンドペーストしたかったんかな <- olでマークアップすべきところをulにしている */ ?>
-<style type="text/css">
-#process li
-{
-	list-style: none;
-}
-</style>
-<ul id="process">
+<ul id="process" class="nomarker">
 	<li>1. 太陽が地上を温める</li>
 	<li>2. 地上の熱を温室効果ガスが吸収して大気が温まる</li>
 	<li>3. 温室効果ガスの濃度と温室効果は比例する</li>
