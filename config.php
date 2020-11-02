a34229ffce58acc35407f5b458acc2cb96a32a1f
jidaikobo <support@jidaikobo.com>
2020-11-02 17:37:38 +0900

    fix description

--- config.php
+++ config.php
@@ -9,27 +9,34 @@ setTestPatternCode(\Kontiki\Input::post('test_pattern_code', ''));
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<title>アクセシビリティ試験練習用サイト設定</title>
+	<title>障壁（バリア）の設定：駒瑠市〜アクセシビリティ上の問題の体験サイト〜</title>
 	<script src="//code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 <body>
-<h1>アクセシビリティ試験練習用サイト設定</h1>
+<h1>障壁（バリア）の設定</h1>
 
 <ul>
-	<li>試験パターンコードを生成します</li>
-	<li>試験パターンコードは1週間Cookieに保存されます</li>
-	<li>試験パターンコードを保存しておくと、あとで同じ状態のサイトで試験できます</li>
-	<li>現在は試験パターンコードの生成はランダム生成のみです</li>
+	<li>障壁パターンコードを生成します</li>
+	<li>障壁パターンコードは1週間Cookieに保存されます</li>
+	<li>障壁パターンコードを保存しておくと、あとで同じ状態のサイトで試験できます</li>
+	<li>開発中のサービスですので、バリアの種類や箇所は随時変更があります。障壁パターンコードがうまく機能しなくなった場合は、設定においてパターンコードを再生成してください</li>
 </ul>
 
-<h2>試験パターンコードを生成</h2>
+<h2>障壁パターンコードを生成</h2>
+<ol>
+	<li>サイトに実装する障壁の設定を行います。ランダム生成か個別指定を選んでください</li>
+	<li>「障壁パターンコードを生成」のボタンを押すと、障壁パターンコードがこのページのtextareaに出力されます</li>
+	<li>「サイトを生成する」を押すことで、設定に応じたサイトが生成されます</li>
+	<li>設定したバリアを使いまわしたい時には、この障壁パターンコードを保存しておいてください。ふたたびこのページのtextareaに貼付することで、おなじ障壁を再現できます</li>
+</ol>
+
 <form action="" method="POST">
 <?php $ng_checked = \Kontiki\Input::post('code_type') != 'individual' ? ' checked="checked"' : ''; ?>
 <ul>
-	<li><label><input type="radio" name="code_type" value="ng"<?php echo $ng_checked ?>> アクセシビリティ上の問題を含んだ試験パターンコードを生成</label></li>
+	<li><label><input type="radio" name="code_type" value="ng"<?php echo $ng_checked ?>> アクセシビリティ上のランダムな問題を含んだ障壁パターンコードを生成</label></li>
 <li>
 <details id="individual_set">
-<summary tabindex="-1"><label><input type="radio" name="code_type" value="individual"> バリアを個別に指定する</label></summary>
+<summary tabindex="-1"><label><input type="radio" name="code_type" value="individual"> 障壁を個別に指定する</label></summary>
 <?php
 $html = '';
 $html.= '<ul>';
@@ -55,21 +62,21 @@ echo $html;
 </li>
 </ul>
 
-<p><input type="submit" name="gen_test_pattern_code" value="試験パターンコードを生成"></p>
+<p><input type="submit" name="gen_test_pattern_code" value="障壁パターンコードを生成"></p>
 </form>
 
-<h2>試験パターンコードがある場合</h2>
+<h2>障壁パターンコードがある場合</h2>
 <form action="" method="POST">
 <?php if (empty($test_pattern_str)): ?>
-<p><label for="test_pattern_code">試験パターンコードを入力してください</label></p>
+<p><label for="test_pattern_code">障壁パターンコードを入力してください</label></p>
 <?php else: ?>
-<p><label for="test_pattern_code">「試験を開始」を押してください</label></p>
+<p><label for="test_pattern_code">「サイトを生成する」を押してください</label></p>
 <?php endif; ?>
 <textarea name="test_pattern_code" id="test_pattern_code" cols="35" rows="7"><?php echo $test_pattern_str ?></textarea>
-<p><input type="submit" value="試験を開始"></p>
+<p><input type="submit" value="サイトを生成する"></p>
 </form>
 
-<p><a href="./">アクセシビリティ試験練習用サイトのトップページ</a></p>
+<p><a href="./">駒瑠市〜アクセシビリティ上の問題の体験サイト〜のトップページ</a></p>
 
 
 <script>
--- index.php
+++ index.php
@@ -2,35 +2,70 @@
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<title>アクセシビリティ試験練習用サイト</title>
+	<title>駒瑠市〜アクセシビリティ上の問題の体験サイト〜</title>
+	<style type="text/css">
+		#readme_vendor li
+		{
+			margin: 0;
+			display: inline-block;
+			vertical-align: top;
+			width: 170px;
+			text-align: center;
+			list-style: none;
+			font-size: 100%;
+		}
+
+		#readme_vendor li img
+		{
+			height: 100px;
+			display: block;
+			margin: 5px auto;
+		}
+
+		footer
+		{
+			border-top: 1px #aaa solid;
+			padding: 25px 0 0;
+		}
+	</style>
 </head>
 <body>
-<h1>アクセシビリティ試験練習用サイト</h1>
+<h1>駒瑠市〜アクセシビリティ上の問題の体験サイト〜</h1>
 <ul>
-	<li>JIS X 8341-3:2016で求められているアクセシビリティの試験を練習するためのサイトです。</li>
-	<li>サイトには意図的にアクセシビリティ上の問題が仕込んであります。</li>
-	<li>以下の「試験の設定を行い試験する」から、練習を行うことができます。</li>
-	<li>フォームの送信後のページなど、試験対象外の箇所には「試験対象外」というテキストが付与されています。</li>
+	<li>意図的なアクセシビリティ上の問題を仕込んだサイトを生成します</li>
+	<li>JIS X 8341-3:2016やWCAGの勉強に使ってもらっても良いですし、「困ったサイト」のサンプルとしてお使いいただいても構いません</li>
+	<li>以下の「障壁の設定を行う」から、サイトの生成に進むことができます</li>
+	<li>フォームの送信後のページなど、対象外の箇所には「試験対象外」というテキストが付与されています</li>
+	<li>無料でお使いいただけます</li>
 </ul>
 
-<h2>試験についての注意事項</h2>
+<h2>注意事項</h2>
 <ul>
-	<li>このサイトは「架空の地方自治体（駒瑠市）のとある課のサイト」という設定でできています。</li>
-	<li>サイトには意図的にアクセシビリティ上の問題が仕込んであります。利用には不快感を伴うことがあります。</li>
-	<li>PDFによる情報提供が存在します。当然ながらHTMLで情報提供した方が良いのですが、このサイトでは、「『お知らせ』はPDFによる添付しかできない困ったCMSを使っている」というような設定だとご理解ください。</li>
-	<li>ブラウザで音声の自動再生をオフにしている場合で、自動再生の不具合を確認したい場合は、ブラウザの設定を調整してください。</li>
-	<li>試験の設定では便宜上達成基準の番号ごとのまとまりを持っていますが、システムの都合上、必ずしもバリアの内容と達成基準の番号は一致しません。</li>
-	<li>意図的にアクセシビリティ上の問題を生成するため、HTMLの文法に誤った箇所が含まれている場合があります。4.1.1の試験に関しては、対象としないようにしてください。</li>
+	<li>このサイトは「架空の地方自治体（駒瑠市・こまるし）のとある課のサイト」という設定でできています</li>
+	<li>サイトには意図的にアクセシビリティ上の問題が仕込んであります。利用には不快感を伴うことがあります</li>
+	<li>PDFによる情報提供が存在します。当然ながらHTMLで情報提供した方が良いのですが、このサイトでは、「『お知らせ』はPDFによる添付しかできない困ったCMSを使っている」というような設定だとご理解ください</li>
+	<li>ブラウザで音声の自動再生をオフにしている場合で、自動再生の不具合を確認したい場合は、ブラウザの設定を調整してください</li>
+	<li>「障壁の設定」では便宜上達成基準の番号ごとのまとまりを持っていますが、システムの都合上、必ずしもバリアの内容と達成基準の番号は一致しません</li>
+	<li>現時点では、障壁はWCAG 2.0 AA（ダブルA）までの問題を実装しています</li>
+	<li>意図的にアクセシビリティ上の問題を生成するため、HTMLの文法に誤った箇所が含まれている場合があります。「4.1.1 構文解析」に関しては、対象としないようにしてください</li>
+	<li>いくつかの達成基準の項目について、障壁が実装されていないものがありますが、「2.3.1 3回の閃光又は閾値以下」の障壁については、将来的にも実装の予定はありません</li>
 </ul>
 
-<h2>実行</h2>
+<h2>障壁の設定へ</h2>
 <ul>
-	<li><a href="./config.php">試験の設定を行い試験する</a></li>
+	<li><a href="./config.php">障壁の設定を行う</a></li>
+</ul>
+
+<h2>問い合わせ等</h2>
+<ul id="readme_vendor">
+	<li><a href="http://www.jidaikobo.com"><img src="./images/logo_author.png" class="a11yc_logo_author" alt="ロゴマーク">有限会社時代工房</a></li>
+	<li><a href="https://twitter.com/jidaikobo"><img src="./images/Twitter_Logo_Blue.png" class="a11yc_logo_author" alt="Twitter Logo">時代工房のTwitter</a></li>
 </ul>
 
 <footer>
 制作：<a href="https:/www.jidaikobo.com">有限会社時代工房</a><br>
 ライセンス：<a href="https://github.com/jidaikobo-shibata/city-komaru/blob/master/LICENSE.txt">MIT</a><br>
+動画・GIFアニメ作成協力：<a href="http://functiontales.com">FUNCTION TALES</a><br>
 使っている音源は<a href="https://www.zapsplat.com">https://www.zapsplat.com</a>で取得しました。
 </footer>
 </body>
